7ccf167f64dd59a0979cd3a41ca7f6abe3622bbd
Nitesh403 <nitesh@hucpl.com>
2024-11-06 15:10:14 +0530

    commant

--- src/components/BestDriver.jsx
+++ src/components/BestDriver.jsx
@@ -15,39 +15,48 @@ const BestDriver = () => {
           </div>
 
           <div className="row bestdrive-drivers">
-            <div className="col-3" style={{ textAlign: "center" }}>
+            <div className="col-lg-3" style={{ textAlign: "center" }}>
               <img
                 src={"/man1.png"}
                 alt="dd"
                 className="bestdrive-drivers-image"
               />
               <div className="bestdrive-drivers-type">
-                <p className="">Shaleie</p>
+                <p className="">PART TIME DRIVER</p>
               </div>
             </div>
 
-            <div className="col-3" style={{ textAlign: "center" }}>
+            <div className="col-lg-3" style={{ textAlign: "center" }}>
               <img
                 src={"/man2.png"}
                 alt="dd"
                 className="bestdrive-drivers-image"
               />
+                <div className="bestdrive-drivers-type">
+                <p className="">FULL TIME DRIVER</p>
+              </div>
             </div>
 
-            <div className="col-3" style={{ textAlign: "center" }}>
+            <div className="col-lg-3" style={{ textAlign: "center" }}>
               <img
                 src={"/man3.png"}
                 alt="dd"
                 className="bestdrive-drivers-image"
               />
+                <div className="bestdrive-drivers-type">
+                <p className="">DRIVER FOR TRIP</p>
+              </div>
             </div>
 
-            <div className="col-3" style={{ textAlign: "center" }}>
+            <div className="col-lg-3" style={{ textAlign: "center" }}>
               <img
                 src={"/man4.png"}
                 alt="dd"
                 className="bestdrive-drivers-image"
               />
+                <div className="bestdrive-drivers-type">
+                <p className="">DRIVER FOR Agreement</p>
+              </div>
             </div>
           </div>
         </div>
